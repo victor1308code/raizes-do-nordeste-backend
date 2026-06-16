9164b190a0ab43ae201252b0b3f8f99b8a62dbbc
Victor Hugo Costa Silva de Oliveira <victor.costa@seac.df.gov.br>
2026-06-16 08:45:43 -0300

    estrutura inicial do projeto

---
+++ src/main/java/com/raizesdonordeste/backend/domain/Unidade.java
@@ -0,0 +1,35 @@
+package com.raizesdonordeste.backend.domain;
+
+public class Unidade {
+
+    private Long id;
+    private String nome;
+    private String endereco;
+
+    public Unidade() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
